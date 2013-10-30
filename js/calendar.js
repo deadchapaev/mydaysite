7daae5430578f92a6c7670ae4050c1af265c4ebe
deadchapaev <chapaevcs@rambler.ru>
2013-10-30 13:51:00 +0200

    Доработал заполнение массива текущими днями

--- js/calendar.js
+++ js/calendar.js
@@ -1,8 +1,8 @@
+//Объект день месяца
 function MyObject(dayNum, dayOfWeak) {
-	this._dayNum = dayNum;
-	this._dayOfWeak = dayOfWeak;
+	this._dayNum = dayNum; //дата
+	this._dayOfWeak = dayOfWeak; //название дня
 };
-
 
 
 //Возвращает массив-заготовку под текущий месяц
@@ -19,50 +19,23 @@ function infillMonth() {
 
 	var month = getArrayOfDays();
 
-	
+	//currenaDay = new Date("21 Dec 2011");
 
-	//dateObj = new Date("21 Dec 2011");
-	dateObj = new Date();
+	//текущая дата
+	var currenaDay = new Date();	
+	//получили первый день месяца
+	var firstDay = new Date(currenaDay.getFullYear(), currenaDay.getMonth(), 1);
+	//firstDay = firstDay.getDate();
 	//получили последний день месяца
-	var lastDay = new Date(dateObj.getFullYear(), dateObj.getMonth() + 1, 0);
+	var lastDay = new Date(currenaDay.getFullYear(), currenaDay.getMonth() + 1, 0);
 	lastDay = lastDay.getDate();
-	//получили день недели
-	var dayOfWeak = dateObj.getDay();
-	//получили день месяца
-	var firstDateOfdayOfWeak = dateObj.getDate()%7;
 	
-	var dayOfWeakForCreate = (dayOfWeak - firstDateOfdayOfWeak + 8)%7;
-	var myObject = new MyObject(1, dayOfWeakForCreate);
+	var shiftDay = firstDay.getDay();
 
-	
 	for (var i = 1; i <= lastDay; i++) {
-		month[Math.ceil((dayOfWeakForCreate + i-1)/7)][i] = new MyObject(i, (myObject._dayOfWeak+i)%7);
+		var nextDay = new MyObject(i, (shiftDay + i-1)%7);
+		month[Math.ceil((nextDay._dayNum + shiftDay)/7)-1][(i)%7] = nextDay;
 	};
-
-	myObject = month[3][4];
-
-
-	var s;
-	if (myObject._dayOfWeak == 0) {
-		s = 'Sun';
-	} else if (myObject._dayOfWeak == 1) {
-		s = 'Mon';
-	} else if (myObject._dayOfWeak == 2) {
-		s = 'Tue';
-	} else if (myObject._dayOfWeak == 3) {
-		s = 'Wen';
-	} else if (myObject._dayOfWeak == 4) {
-		s = 'The';
-	} else if (myObject._dayOfWeak == 5) {
-		s = 'Fri';
-	} else if (myObject._dayOfWeak == 6) {
-		s = 'Sat';
-	};
-
-
-
-
-	alert(s);
 
 	return month;
 
