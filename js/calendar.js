0ae15cc221b5c7c859f06db6d10b516842de4a89
chapaev <chapaevcs@rambler.ru>
2013-11-10 02:14:19 +0200

    Глобальные правки скрипта календаря

--- js/calendar.js
+++ js/calendar.js
@@ -50,6 +50,7 @@ function operation(month){
 
     var curTable =  document.getElementsByClassName('calendargrid')[0];
 
+
 	for (var row_i = 0;  row_i <= curTable.rows.length;  row_i++) {
 		for (var col_j = 0; col_j <= curTable.rows[row_i].cells.length; col_j++) {
 			var curDay = month[row_i][col_j];
@@ -63,9 +64,25 @@ function operation(month){
 	}
 }
 
+function operation1(day){
+
+	var curTable =  document.getElementsByClassName('calendargrid')[0];
+	var rows = curTable.rows;
+	var maxI = day.getLastDay().getDate();
+
+	day = new Day(day.getFirstDay());
+	for (var i = 1; i <= maxI;  i++) {
+		var curRow = rows[day.getWeekNum()];
+		var cell = curRow.cells[day.getDayNum()];
+		cell.innerHTML = day.getDay(); 
+		day = day.getNextDay(); 
+	}
+}
+
 $(document).ready(function(){
 	var currentDay = new Date();	
-	currentDay = new Date("21 Jan 2013");
-	var month = infillMonth(currentDay);
-	operation(month);
+	currentDay = new Date("23 Aug 2013");
+	//var month = infillMonth(currentDay);
+	var day = new Day(currentDay);
+	operation1(day);
 });
