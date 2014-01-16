b98e6dbe6ac81f4246361db062a5d978b80f9a02
jjackass <jonnyjackass@qip.ru>
2014-01-16 16:03:07 +0200

    egrhtjkykli

--- application/view/ViewMain.php
+++ application/view/ViewMain.php
@@ -1,15 +1,18 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" type="text/css" href="calendar.css">
-<link rel="stylesheet" type="text/css" href="auth.css">\
-<link rel="stylesheet" type="text/css" href="addevent.css">
-<title>MakeMy</title>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>MakeMy</title>
+
 <script type="text/javascript" src="js/jquery-1.3.min.js" charset="utf-8"></script>
 <script type="text/javascript" src="js/menu3.js" charset="utf-8"></script>
 <script type="text/javascript" src="js/calendar2.js" charset="utf-8"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="calendar.css">
+<link rel="stylesheet" type="text/css" href="auth.css">
+<link rel="stylesheet" type="text/css" href="addevent.css">
 </head>
 <body>
 <div class="container">
