2a8d5ed5842f5b263e5146d29525d3337ba5965b
deadchapaev <chapaevcs@rambler.ru>
2014-01-22 14:43:22 +0200

    Подправил вызов вьюхи

--- application/controller/class.ControllerEvent.php
+++ application/controller/class.ControllerEvent.php
@@ -1,20 +1,20 @@
 <?php
-require_once 'application/core/class.Controller.php';
+require_once '/application/core/class.Controller.php';
 class ControllerEvent extends Controller
 {
     function actionDefault()
     {
-        $this->view->generate('ViewEvent.php', 'ViewMain.php', null);
+        $this->getView()->generate('ViewEvent.php', 'ViewMain.php', null);
     }
 
     function actionAdd()
     {
-        $this->view->generate('ViewEventAdd.php', 'ViewMain.php', null);
+        $this->getView()->generate('ViewEventAdd.php', 'ViewMain.php', null);
     }
 
     function actionAddGroup()
     {
-        $this->view->generate('ViewGroupAdd.php', 'ViewMain.php', null);
+        $this->getView()->generate('ViewGroupAdd.php', 'ViewMain.php', null);
     }
 }
 
