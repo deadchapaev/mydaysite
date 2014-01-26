7bc61f077f7096e95460f9452bf24ea1416632cd
deadchapaev <chapaevcs@gmail.com>
2014-01-26 21:05:47 +0200

    Играюсь

--- js/menu.js
+++ js/menu.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
                 $(this).removeClass('checked');
             }
         });
+
         //скорем-покажем выбранную группу
         $(".userspace").each(function (index) {
             if (null != $groupid) {
