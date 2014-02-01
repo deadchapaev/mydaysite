295209eb5e3b1b3c9b31a8f1893f724206d1c280
jjackass <jonnyjackass@qip.ru>
2014-02-01 12:59:11 +0200

    сверстан футер! но на 80%

--- application/view/ViewFooter.php
+++ application/view/ViewFooter.php
@@ -1,19 +1,84 @@
 <div class="footer">
-    <ul id="navigation-footer">
-        <li class="main"><a href="/">ГЛАВНАЯ</a></li>
-        <li class="contacts"><a href="#">КОНТАКТЫ</a></li>
+    <div class="footer-top">
 
-        <?php
-        if (null !== $data['user'] && null !== $data['user']->id) {
-            ?>
-            <li class="exit"><a href="/User/Logout.ws">ВЫЙТИ</a></li>
-        <?php
-        } else {
-            ?>
-            <li class="exit"><a href="/User/">ВОЙТИ</a></li>
-        <?php
-        }
-        ?>
-        <li class="about"><a href="#">О НАС</a></li>
-    </ul>
+        <div class="learn-area">
+            <div class="learn-label">
+                MakeMyDay - начни легко
+            </div>
+
+            <div class="learn-text">
+                Используйте наш сервис чтобы ничего не упустить
+            </div>
+
+            <div class="footer-button">
+                <ul class=learn-button>
+                    <a href="/">
+                        <li>Узнать больше</li>
+                    </a>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        <div class="footer-bottom-left">
+            <div class="footer-cont-label">
+                Обратная Связь
+            </div>
+
+            <div class="footer-contacts">
+                <ul>
+                    <li>
+                        <img src="/img/mailicon.png"> mmd@google.com
+                    </li>
+                    <li>
+                        <img src="/img/icqicon.png"> 400747128
+                    </li>
+                    <li>
+                        <img src="/img/skypeicon.png"> makeMyDay
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom-center">
+            <div class="footer-cont-label-center">
+                Новости проекта
+            </div>
+            <div class="footer-cont-text-center">
+                В данный момент наш сайт находится в стадии разработки, но уже в скором времени вы сможете увидеть альфа-версию и даже немного потестировать
+            </div>
+
+        </div>
+
+        <div class="footer-bottom-right">
+
+        </div>
+    </div>
+    <div class=footer-end>
+        <div class="navigation-footer">
+            <ul>
+
+                <li class="main"><a href="/">ГЛАВНАЯ</a></li>
+                <li class="contacts"><a href="#">КОНТАКТЫ</a></li>
+
+                <?php
+                if (null !== $data['user'] && null !== $data['user']->id) {
+
+                    ?>
+                    <li class="exit"><a href="/User/Logout.ws">ВЫЙТИ</a></li>
+                <?php
+                } else {
+
+                    ?>
+                    <li class="exit"><a href="/User/">ВОЙТИ</a></li>
+                <?php
+                }
+
+                ?>
+                <li class="about"><a href="#">О НАС</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
