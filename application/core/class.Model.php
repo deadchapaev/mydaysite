3dc2734cb8a99992797ab4fca2a70c26198a2e90
deadchapaev <chapaevcs@gmail.com>
2014-02-17 23:16:17 +0200

    Убрал ненужные комменты

--- application/core/class.Model.php
+++ application/core/class.Model.php
@@ -5,33 +5,21 @@ class Model
     private $inputVarArray;
     private $user;
 
-    /**
-     * @return mixed
-     */
     public function getInputVarArray()
     {
         return $this->inputVarArray;
     }
 
-    /**
-     * @param mixed $inputVarArray
-     */
     public function setInputVarArray($inputVarArray)
     {
         $this->inputVarArray = $inputVarArray;
     }
 
-    /**
-     * @return mixed
-     */
     public function getUser()
     {
         return $this->user;
     }
 
-    /**
-     * @param mixed $user
-     */
     public function setUser($user)
     {
         $this->user = $user;
