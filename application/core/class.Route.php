129e6305c43390ca8c55ad4d5fef11887e804cee
deadchapaev <chapaevcs@gmail.com>
2014-03-27 22:00:40 +0200

    Добавил путь под апи...

--- application/core/class.Route.php
+++ application/core/class.Route.php
@@ -20,57 +20,58 @@ class Route
         if (!empty($routes[1])) {
             $controller_name = $routes[1];
         }
+        if ((strcasecmp($controller_name, "api") == 0)) {
+            //TODO: здесь будет работа с апи
 
-        // получаем имя экшена
-        if (!empty($routes[2])) {
-            $action_name = $routes[2];
-            if ((strrpos($action_name, '?'))) {
-                $action_name = substr($action_name, 0, strrpos($action_name, '?'));
+        } else {
+            // получаем имя экшена
+            if (!empty($routes[2])) {
+                $action_name = $routes[2];
+                if ((strrpos($action_name, '?'))) {
+                    $action_name = substr($action_name, 0, strrpos($action_name, '?'));
+                }
+                if ((strrpos($action_name, '.ws'))) {
+                    $action_name = str_replace('.ws', 'Ws', $action_name);
+                }
             }
-            if ((strrpos($action_name, '.ws'))) {
-                $action_name = str_replace('.ws', 'Ws', $action_name);
+
+            // подцепляем файл с классом модели (файла модели может и не быть)
+            $model_path = "application/model/class.Model" . $controller_name . '.php';
+            if (file_exists($model_path)) {
+                include_once $model_path;
             }
-        }
 
-        // добавляем префиксы
-        $model_name = 'class.Model' . $controller_name;
-        $controller_name = 'Controller' . $controller_name;
-        $action_name = 'action' . $action_name;
+            // подцепляем файл с классом контроллера
+            $controller_name = 'Controller' . $controller_name;
+            $controller_path = "application/controller/class." . $controller_name . '.php';
+            if (file_exists($controller_path)) {
 
-        // подцепляем файл с классом модели (файла модели может и не быть)
-        $model_file = $model_name . '.php';
-        $model_path = "application/model/" . $model_file;
-        if (file_exists($model_path)) {
-            include_once $model_path;
-        }
+                include_once $controller_path;
 
-        // подцепляем файл с классом контроллера
-        $controller_file = 'class.' . $controller_name . '.php';
-        $controller_path = "application/controller/" . $controller_file;
-        if (file_exists($controller_path)) {
+                // создаем контроллер
+                $controller = new $controller_name;
+                $action = 'action' . $action_name;
+                if (method_exists($controller, $action)) {
+                    // вызываем действие контроллера
+                    $controller->init();
+                    $controller->$action();
+                } else {
+                    Route::ErrorPage404();
+                }
 
-            include_once $controller_path;
-
-            // создаем контроллер
-            $controller = new $controller_name;
-            $action = $action_name;
-            if (method_exists($controller, $action)) {
-                // вызываем действие контроллера
-                $controller->init();
-                $controller->$action();
             } else {
                 Route::ErrorPage404();
             }
 
-        } else {
-            Route::ErrorPage404();
         }
+
+
     }
 
     function ErrorPage404()
     {
         $data['msg'] = 'Страница не найдена!';
-        $controllerInfo  = new ControllerInfo();
+        $controllerInfo = new ControllerInfo();
         $controllerInfo->setData($data);
         $controllerInfo->actionError();
     }
